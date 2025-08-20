3acaecd9fa2af34790a21fa526f9541781ab856c
PavanKumar K R <pavan123456pop@gmail.com>
2025-08-20 12:57:02 +0530

    add server folder for server actions

---
+++ server/users.ts
@@ -0,0 +1,21 @@
+"use server";
+import { auth } from "@/lib/auth";
+
+export const signIn = async () => {
+  await auth.api.signInEmail({
+    body: {
+      email: "user@email.com",
+      password: "password",
+    },
+  });
+};
+
+export const signUp = async () => {
+  await auth.api.signUpEmail({
+    body: {
+      email: "user@email.com",
+      password: "password",
+      name: "user",
+    },
+  });
+};
